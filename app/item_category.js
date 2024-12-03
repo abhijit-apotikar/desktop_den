b9871c3fd39c29b7dac305fb1f91f67bd86a5bc2
Abhijit Apotikar <abhijit.apotikar156@gmail.com>
2024-12-03 13:01:40 +0530

    update: make ItemCategory component consume pc_marketplace endpoint

--- app/item_category.js
+++ app/item_category.js
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 export default function ItemCategory(){
+    const [categoryName, setCategoryName] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:4000');
+                setCategoryName(response.data);
+            } catch (error) {
+                setError(error.message);
+            }
+        }
+
+        fetchData();
+    }, []);
+
 
     return (<div>
-        <p>Item Category</p>
+        <h1>
+            {error && <p>Error: {error}</p>}
+            {categoryName ? <pre>{JSON.stringify(categoryName.message, null, 2)}</pre> : <p>Loading ...</p>}
+        </h1>
     </div>);
 };
